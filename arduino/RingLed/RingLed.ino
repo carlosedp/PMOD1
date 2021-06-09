93ecc409b23ec58b0deb57acd1a4a4578897c6e7
Carlos de Paula <me@carlosedp.com>
2021-06-09 10:14:06 -0300

    Update Arduino sample with encoder

--- arduino/RingLed/RingLed.ino
+++ arduino/RingLed/RingLed.ino
@@ -1,11 +1,25 @@
+#define ENCODER_DO_NOT_USE_INTERRUPTS
+#include <Encoder.h>
+
 // Using NodeMCU pins
+// RingLED Pins
 const int dataPin = D6;   //Outputs the byte to transfer
 const int loadPin = D8;   //Controls the internal transference of data in SN74HC595 internal registers
 const int clockPin = D7;  //Generates the clock signal to control the transference of data
 const int BankA = D3;     // Selects BankA - LEDs 1-8
 const int BankB = D4;     // Selects BankB - LEDs 9-16
-const int del = 50;
+long interval = 200;
+
+// Encoder Pins
+int encoder0PinA = D1;
+int encoder0PinB = D2;
+int encoder0PinSW = D0;
+
+Encoder myEnc(encoder0PinA, encoder0PinB);
+long position  = -999;
+
 void setup() {
+  Serial.begin (115200);
   pinMode(dataPin, OUTPUT);
   pinMode(loadPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
@@ -16,21 +30,47 @@ void setup() {
   digitalWrite(BankB, LOW);
 }
 
+void rotEncoder() {
+  long newPos = myEnc.read();
+  if (newPos != position) {
+    if (newPos > position) {
+       Serial.print("Clockwise: ");
+       interval = interval + 1;
+    } else {
+      if (interval >= 4) {
+        Serial.print("Counter Clockwise: ");
+        interval = interval - 1;
+      }
+    }
+    Serial.print(interval);
+    Serial.print(" - ");
+    Serial.println(position);
+    position = newPos;
+  }
+}
+
 unsigned long integerData = 1;
 unsigned long total = 1;
+void ringLED() {
+  static long currentMillis;
+  if (millis() - currentMillis >= interval) {
+    if (total <=  128) {digitalWrite(BankA, HIGH);digitalWrite(BankB, LOW);}
+    if (total >  128) {digitalWrite(BankA, LOW);digitalWrite(BankB, HIGH);}
+    // Send data to IC
+    digitalWrite(loadPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, integerData);
+    digitalWrite(loadPin, HIGH);
+    // Shift bits
+    integerData = integerData << 1;
+    total = total << 1;
+    // Wrap around
+    if (integerData > 128) {integerData = 1;}
+    if (total > 65535) {total = 1;}
+    currentMillis = millis();
+   }
+}
 
 void loop() {
-  if (total <=  128) {digitalWrite(BankA, HIGH);digitalWrite(BankB, LOW);}
-  if (total >  128) {digitalWrite(BankA, LOW);digitalWrite(BankB, HIGH);}
-  // Send data to IC
-  digitalWrite(loadPin, LOW);
-  shiftOut(dataPin, clockPin, LSBFIRST, integerData);
-  digitalWrite(loadPin, HIGH);
-  // Shift bits
-  integerData = integerData << 1;
-  total = total << 1;
-  // Wrap around
-  if (integerData > 128) {integerData = 1;}
-  if (total > 65535) {total = 1;}
-  delay(del); 
+  ringLED();
+  rotEncoder();
 }
